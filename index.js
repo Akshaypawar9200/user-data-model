263109b4dc00dd1763e2724d571c57b47b3b79b8
unknown <akshaypawar9200@gmail.com>
2024-06-30 12:03:27 +0530

    changes in data model completed

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const env = process.env.NODE_ENV || "local";
+const requiredEnvVariables = [
+    "NODE_ENV",
+    "USER_USERNAME",
+    "USER_DB_PASSWORD",
+    "USER_DB_NAME",
+   " USER_DB_INSTANCE_URL",
+    "USER_DB_PORT"
+];
+for (const envVar of requiredEnvVariables) {
+  if (!process.env[envVar]) {
+    throw new Error(
+      `${envVar} is required but not found in the environment variables`
+    );
+  }
+}
 const db = require("./models");
 
 module.exports = db;
